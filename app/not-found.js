1b2f12ba10eb650457cbe97a871bd25f06d72dd6
dayana.loaiza <dayanalr1992@gmail.com>
2024-06-06 14:46:05 -0600

    404 page added

---
+++ app/not-found.js
@@ -0,0 +1,24 @@
+import '/app/styles/main.scss';
+import { oswald } from './fonts';
+
+const NotFound = () => {
+  return (
+    <main className="container-fluid vh-100">
+      <div className='row w-100 h-100'>
+        <div className='col p-0' 
+          style={{
+            backgroundImage: "url('/images/cover-bg.jpg')",
+            backgroundSize: "cover"
+          }}
+          >
+        </div>
+        <div className='col centered flex-column p-0'>
+          <h1 className={`section-title ${oswald.className}`}>404<br/> Not Found</h1>
+          <a href="/#" className="btn btn-secondary">Back to home</a>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default NotFound;
